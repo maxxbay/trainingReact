8b94aa63376014c9794d2d0ad496b2b91c882e64
Maxxbay <maxxbay@gmail.com>
2022-10-31 13:15:48 +0100

    Update Posts.js

--- src/components/17-json-placeholder-post/src/components/Posts.js
+++ src/components/17-json-placeholder-post/src/components/Posts.js
@@ -1,13 +1,15 @@
 import Post from './Post';
 import { useEffect, useState } from 'react';
 
+const API_URL = 'https://jsonplaceholder.typicode.com/posts';
+
 function Posts() {
   const [posts, setPosts] = useState([]);
   const [error, setError] = useState('');
   const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+    fetch(API_URL)
       .then(res => res.json())
       .then(posts => setPosts(posts))
       .catch(error => setError(error.message))
