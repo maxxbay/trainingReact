370e1ff1b9d31b5ec99cf562b36038bb382ac1d7
Maxxbay <maxxbay@gmail.com>
2022-10-31 13:11:11 +0100

    posts

---
+++ src/components/17-json-placeholder-post/src/components/Posts.js
@@ -0,0 +1,33 @@
+import Post from './Post';
+import { useEffect, useState } from 'react';
+
+function Posts() {
+  const [posts, setPosts] = useState([]);
+  const [error, setError] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(posts => setPosts(posts))
+      .catch(error => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, []);
+
+  if (error) {
+    return <h1>Error: {error}</h1>;
+  }
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading ...</h1>
+      ) : (
+        posts.map(post => <Post key={post.id} {...post} />)
+      )}
+    </>
+  );
+}
+export default Posts;
